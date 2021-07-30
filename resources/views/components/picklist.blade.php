446153a30ffd40c06c5ccefb39dd6d7209852ebf
salesfore-casts <kkteja@gmail.com>
2021-07-30 07:49:12 +0530

    alternative approach for picklist component

--- resources/views/components/picklist.blade.php
+++ resources/views/components/picklist.blade.php
@@ -6,7 +6,7 @@
     <option>--None--</option>
 
     @foreach ($value as $key => $item)
-        <option value="{{ $key }}" {{ ($selected == $key) ? 'selected' : '' }}>
+        <option value="{{ $key }}" @if($selected == $key) selected @endif>
             {{ $item }}
         </option>
     @endforeach
