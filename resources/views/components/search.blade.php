1cc56ca0cb7afad678b4a34b6d434c2a40a59e0e
salesfore-casts <teja@salesforcecasts.com>
2021-08-12 15:10:45 +0530

    dummy

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -16,21 +16,21 @@
                 </div>
             </div>
             <!--
-  This example requires Tailwind CSS v2.0+
+            This example requires Tailwind CSS v2.0+
 
-  This example requires some changes to your config:
+            This example requires some changes to your config:
 
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
+            ```
+            // tailwind.config.js
+            module.exports = {
+                // ...
+                plugins: [
+                // ...
+                require('@tailwindcss/forms'),
+                ],
+            }
+            ```
+            -->
             <div class="mt-4" style="width: 50%">
                 <x-label for="Type" :value="__('Type')" />
 
