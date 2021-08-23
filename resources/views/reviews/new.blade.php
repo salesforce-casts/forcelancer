bcf4ecce99081d1ef9bbe10f9dfb49fd7a8818e8
salesfore-casts <teja@salesforcecasts.com>
2021-08-23 16:40:52 +0530

    Working Review System

--- resources/views/reviews/new.blade.php
+++ resources/views/reviews/new.blade.php
@@ -1,4 +1,7 @@
 <x-app-layout>
+    <x-slot name="scripts">
+        <script src="https://unpkg.com/vue@next"></script>
+    </x-slot>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Post a Review') }}
@@ -39,17 +42,18 @@
                                 :value="old('review')" required autofocus />
                         </div>
                         <div class="mt-4">
-                            <div class="flex" v-for="i in rating" id="cs-rating">
 
-                                1
-                                <img src="https://via.placeholder.com/150" />
-                                {{-- <button type="button" v-for="i in 5" :class="{ 'mr-1': i < 5 }">
-                                    <svg class="block h-8 w-8" :class="[ rating >= i ? 'text-blue': 'text-grey']"
-                                        fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+
+                            <div class="flex" v-for="i in 5" id="cs-rating">
+                                <button type="button" v-for="i in 5" :class="{ 'mr-1': i < 5 }" @click="handleReview(i)"
+                                    :key="i">
+                                    <svg class="block h-8 w-8 fill-current"
+                                        :class="[ rating >= i ? 'text-yellow-500': 'text-gray-400']"
+                                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                         <path
                                             d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
                                     </svg>
-                                </button> --}}
+                                </button>
                             </div>
                             {{-- <div class="flex justify-center items-center">
                                 <div class="flex items-center mt-2 mb-4">
@@ -57,27 +61,27 @@
                                         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                         <path
                                             d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-                                        </svg>
+                                    </svg>
                                     <svg class="mx-1 w-4 h-4 fill-current text-yellow-500"
                                         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                         <path
                                             d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-                                        </svg>
+                                    </svg>
                                     <svg class="mx-1 w-4 h-4 fill-current text-yellow-500"
                                         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                         <path
                                             d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-                                        </svg>
+                                    </svg>
                                     <svg class="mx-1 w-4 h-4 fill-current text-yellow-500"
                                         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                         <path
                                             d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-                                        </svg>
+                                    </svg>
                                     <svg class="mx-1 w-4 h-4 fill-current text-gray-400"
                                         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                         <path
                                             d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-                                        </svg>
+                                    </svg>
                                 </div>
                             </div> --}}
                         </div>
@@ -95,11 +99,14 @@
         var app = Vue.createApp({
             data() {
                 return {
-                    rating: 3,
+                    rating : 3,
                 }
             },
             methods: {
-
+                handleReview (rating) {
+                    console.log(rating);
+                    this.rating = rating;
+                }
             }
         });
         const vm  = app.mount('#cs-rating');
