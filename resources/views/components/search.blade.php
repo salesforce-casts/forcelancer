4b8205c0eb14656ddcad09519d7847a361f3e008
salesfore-casts <teja@salesforcecasts.com>
2022-02-09 16:33:21 +0530

    Working route with vue variable

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -128,8 +128,7 @@
                     @{{ resource.name }}
                     @{{ resource.describe }}
 
-                    {{-- https://laracasts.com/discuss/channels/vue/add-route-parameter-with-vuejs --}}
-                    <a href="/profile/show/@{{ resource.id }}" class="cursor-pointer"> read more ...</a>
+                    <a :href="'/profile/show/' + resource.id" class="cursor-pointer"> read more ...</a>
                 </div>
             </div>
         </div>
