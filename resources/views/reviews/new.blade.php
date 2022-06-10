dbd98bf8ce1a940ab4ec8049973f766b8960696b
Anuj <anujjavajob@gmail.com>
2022-06-10 12:03:16 +0530

    Dynamic resourve name and time period added in review page

--- resources/views/reviews/new.blade.php
+++ resources/views/reviews/new.blade.php
@@ -11,6 +11,22 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="mb-3">
+                        <h1>
+                            You are trying to provide review for 
+                            <b>{{ $hirerResource->resource->name }} </b>
+                            whom you hired for 
+                            <b>
+                                @if($hirerResource->monthly)
+                                    1 month.
+                                @elseif($hirerResource->weekly)
+                                    1 week.
+                                @elseif($hirerResource->hours)
+                                    {{ $hirerResource->hours  }} hours.
+                                @endif
+                            </b>
+                        </h1>
+                    </div>
 
                     @if ($errors->any())
 
