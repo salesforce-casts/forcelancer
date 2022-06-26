72046916010eb41957db9b2849d72bdb11982a85
Anuj <anujjavajob@gmail.com>
2022-06-26 17:19:47 +0530

    resourece profile review listing bug fixed

--- resources/views/resources/show.blade.php
+++ resources/views/resources/show.blade.php
@@ -322,13 +322,15 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
                 <div>
                     <div class="flex items-center">
-                        <h1 class="text-2xl">Reviews</h1>
+                        <h1 class="text-2xl mr-3">Reviews</h1>
                         @for ($i = 0; $i < 5 ; $i++)
                             <svg class="w-5 h-5 {{ (floor($overAllRating->avg_rating) > $i)? 'text-yellow-400' : 'text-gray-300 dark:text-gray-500' }}" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                         @endfor
-                        <p class="ml-2 text-sm font-medium text-gray-900 dark:text-white">{{ $overAllRating->avg_rating }} out of 5</p>
+                        <p class="ml-2 text-sm font-medium text-gray-900 dark:text-white">{{ $overAllRating->avg_rating ? $overAllRating->avg_rating : 0 }} out of 5</p>
                     </div>
-                    <p class="text-sm font-medium text-gray-500 dark:text-gray-400">{{ $overAllRating->total_review }} global ratings</p>
+                    @if($overAllRating->total_review)
+                        <p class="text-sm font-medium text-gray-500 dark:text-gray-400">{{ $overAllRating->total_review }} global ratings</p>
+                    @endif
                 </div>
                 @forelse ($reviews as $review)
                     <div class="my-4 border-b border-solid border-red-600">                   
@@ -364,7 +366,7 @@
                         </article>
                     </div>
                 @empty
-                    <h1>No review yet!!!</h1>
+                    <h1>No review yet !</h1>
                 @endforelse
             </div>
         </div>
