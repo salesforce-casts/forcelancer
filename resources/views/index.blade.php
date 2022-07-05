0002451b6e71fb1db67f0966e488063e90e45084
salesfore-casts <teja@salesforcecasts.com>
2022-07-05 18:03:23 +0530

    commented fixed header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,23 +21,23 @@
 
 <body class="antialiased">
 <x-hero-section></x-hero-section>
-<div
-    class="relative flex items-top justify-center min-h-screen bg-white sm:items-center py-4 sm:pt-0">
-    @if (Route::has('login'))
-        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            @auth
-                <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
-            @else
-                <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+{{--<div--}}
+{{--    class="relative flex items-top justify-center min-h-screen bg-white sm:items-center py-4 sm:pt-0">--}}
+{{--    @if (Route::has('login'))--}}
+{{--        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">--}}
+{{--            @auth--}}
+{{--                <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>--}}
+{{--            @else--}}
+{{--                <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>--}}
 
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                @endif
-            @endauth
-        </div>
-    @endif
+{{--                @if (Route::has('register'))--}}
+{{--                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>--}}
+{{--                @endif--}}
+{{--            @endauth--}}
+{{--        </div>--}}
+{{--    @endif--}}
 
-</div>
+{{--</div>--}}
 <x-page-section></x-page-section>
 <x-faq></x-faq>
 <x-cta></x-cta>
