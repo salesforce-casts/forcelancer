32146103d36dce75b2980ec08366b30c61e49355
salesfore-casts <teja@salesforcecasts.com>
2022-07-05 18:00:41 +0530

    removed search from home page!

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,29 +20,28 @@
 </head>
 
 <body class="antialiased">
-    <x-hero-section></x-hero-section>
-    <div
-        class="relative flex items-top justify-center min-h-screen bg-white sm:items-center py-4 sm:pt-0">
-        @if (Route::has('login'))
+<x-hero-section></x-hero-section>
+<div
+    class="relative flex items-top justify-center min-h-screen bg-white sm:items-center py-4 sm:pt-0">
+    @if (Route::has('login'))
         <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
             @auth
-            <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
             @else
-            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+                <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
 
-            @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-            @endif
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                @endif
             @endauth
         </div>
-        @endif
-        <x-search></x-search>
+    @endif
 
-    </div>
-    <x-page-section></x-page-section>
-    <x-faq></x-faq>
-    <x-cta></x-cta>
-    <x-footer></x-footer>
+</div>
+<x-page-section></x-page-section>
+<x-faq></x-faq>
+<x-cta></x-cta>
+<x-footer></x-footer>
 </body>
 
 
