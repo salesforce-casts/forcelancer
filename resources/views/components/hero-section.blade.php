2c523acf862ba3eb03bf1712f0628410a2b8ffc4
salesfore-casts <teja@salesforcecasts.com>
2022-07-05 18:08:52 +0530

    minor changes to homepage nav link

--- resources/views/components/hero-section.blade.php
+++ resources/views/components/hero-section.blade.php
@@ -53,11 +53,6 @@
                         </div>
                     </div>
                     <div class="hidden md:flex md:space-x-10">
-                        <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Product</a>
-
-                        <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Features</a>
-
-                        <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Marketplace</a>
 
                         <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Company</a>
 
@@ -65,7 +60,7 @@
                     </div>
                     <div class="hidden md:absolute md:flex md:items-center md:justify-end md:inset-y-0 md:right-0">
               <span class="inline-flex rounded-md shadow">
-                <a href="#"
+                <a href="{{route('login')}}"
                    class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50"> Log in </a>
               </span>
                     </div>
@@ -103,19 +98,15 @@
                         </div>
                     </div>
                     <div class="px-2 pt-2 pb-3">
-                        <a href="#"
-                           class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Product</a>
 
-                        <a href="#"
-                           class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Features</a>
-
-                        <a href="#"
-                           class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Marketplace</a>
 
                         <a href="#"
                            class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Company</a>
+                        <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900
+                        hover:bg-gray-50">Search</a>
+
                     </div>
-                    <a href="#"
+                    <a href="{{route('login')  }}"
                        class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100">
                         Log in </a>
                 </div>
