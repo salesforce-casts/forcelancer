21a0eafbcbc6cacb2a8a15d7f8f161988c176b03
Anuj <anujjavajob@gmail.com>
2022-07-06 21:01:57 +0530

    nav-bar added in search resource page and profile login dropdown not visible for guest user

--- resources/views/search/resource.blade.php
+++ resources/views/search/resource.blade.php
@@ -1,8 +1,8 @@
 
-<x-guest-layout>
+<x-semi-app-layout>
     
     <div class="relative flex items-top justify-center min-h-screen bg-white sm:items-center py-4 sm:pt-0">        
         <x-search></x-search>
 
     </div>
-</x-guest-layout>
+</x-semi-app-layout>
